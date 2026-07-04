667f9ccef6dee879c9c5b2def4c8bc07665d58d2
Martino <72280379+martinopiaggi@users.noreply.github.com>
2026-07-04 17:52:42 +0200

    chore: release v0.2.2

--- summarizer/__init__.py
+++ summarizer/__init__.py
@@ -5,5 +5,5 @@ from .exceptions import (
     ConfigurationError, AudioProcessingError, SourceNotFoundError
 )
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __all__ = ["main", "CONFIG", "SummarizerError"]
